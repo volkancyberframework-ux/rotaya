f12177f8dea363d37fb64d33977b6ee28f0236e5
volkanguler <volkanguler@Mac.lan>
2026-05-09 16:44:13 +0700

    Fix media file serving on Render

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 from core.views import (
     home,
@@ -41,7 +41,10 @@ urlpatterns = [
 
 
 # Render Disk media files
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+urlpatterns += [
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
+]
